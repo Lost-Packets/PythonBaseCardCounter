a44b2bbae5b279b924446456eabcc962e5f4097e
Nathan Pva <82654348+Lost-Packets@users.noreply.github.com>
2022-04-01 14:26:44 +1300

    Added additional stats

--- CardCounter.py
+++ CardCounter.py
@@ -1,4 +1,3 @@
-
 class Card:
     def __init__(self, suit, val):
         self.suit = suit
@@ -71,27 +70,49 @@ class Deck:
 class CountingLog(Deck):
     def __init__(self, deckCount):
         super().__init__(deckCount)
+        #properties
         self.runningCount = 0
         self.trueCount = 0
         self.DefualtDeck = self.Cards
+        self.low = self.Cards[0:8] + self.Cards[13:20] + self.Cards[26:33] + self.Cards[39:50]  
+        self.high = self.Cards[8:13] + self.Cards[21:26] + self.Cards[34:39] + self.Cards[47:53]
+
+        # 8 decks
+        self.totalHigh = 200
+        self.totalLow = 200
+        self.total = 416
+
+        # 6 decks
+        self.totalHigh6 = 150
+        self.totalLow6 = 150
+        self.total6 = 312
+
+        # 4 decks
+        self.totalHigh4 = 100
+        self.totalLow4 = 100
+        self.total4 = 208
+
+        # 2 decks
+        self.totalHigh2 = 50
+        self.totalLow2 = 50
+        self.total2 = 104
         
     def assignment(self, card):
         for card in self.FinalDeck:
             if card == card:
                 self.FinalDeck.pop()
-                break;
+                break;  
 
-        low = self.Cards[0:8] + self.Cards[13:20] + self.Cards[26:33] + self.Cards[39:50]  
-        high = self.Cards[8:13] + self.Cards[21:26] + self.Cards[34:39] + self.Cards[47:53]  
-
-        if card in low:
+        if card in self.low:
             self.runningCount = self.runningCount + 1
 
-        elif card in high:
+        elif card in self.high:
             self.runningCount = self.runningCount - 1
 
         else:
             self.runningCount = self.runningCount + 0
+        
+        self.countingStats(card)
  
     def TrueCount(self):
         self.trueCount = self.runningCount / round(len(self.FinalDeck) / 52)
@@ -101,6 +122,48 @@ class CountingLog(Deck):
     def RunningCOunt(self):
         print("Running Count : {}".format(self.runningCount))
 
+    def countingStats(self, card):
+        if self.deckCount == 8:
+            if card in self.high:
+                self.totalHigh = self.totalHigh - 1
+                self.total = self.total - 1
+            elif card in self.low:
+                self.totalLow = self.totalLow - 1
+                self.total = self.total - 1
+            print("% Of Next Card Being High: ", round(((self.totalLow / self.total)*100), 2), "%" )
+            print("% Of Next Card Being Low: ", round(((self.totalHigh / self.total)*100), 2), "%" )
+
+        elif self.deckCount == 6:
+            if card in self.high:
+                self.totalHigh6 = self.totalHigh6 - 1
+                self.total6 = self.total6 - 1
+            elif card in self.low:
+                self.totalLow6 = self.totalLow6 - 1
+                self.total6 = self.total6 - 1
+            print("% Of Next Card Being High: ", round(((self.totalLow6 / self.total6)*100), 2), "%" )
+            print("% Of Next Card Being Low: ", round(((self.totalHigh6 / self.total6)*100), 2), "%" )
+
+        elif self.deckCount == 4:
+            if card in self.high:
+                self.totalHigh4 = self.totalHigh4 - 1
+                self.total4 = self.total4 - 1
+            elif card in self.low:
+                self.totalLow4 = self.totalLow4 - 1
+                self.total4 = self.total4 - 1
+            print("% Of Next Card Being High: ", round(((self.totalLow4 / self.total4)*100), 2), "%" )
+            print("% Of Next Card Being Low: ", round(((self.totalHigh4 / self.total4)*100), 2), "%" )
+
+        elif self.deckCount == 2:
+            if card in self.high:
+                self.totalHigh2 = self.totalHigh2 - 1
+                self.total2 = self.total2 - 1
+            elif card in self.low:
+                self.totalLow2 = self.totalLow2 - 1
+                self.total2 = self.total2 - 1
+            print("% Of Next Card Being High: ", round(((self.totalLow2 / self.total2)*100), 2), "%" )
+            print("% Of Next Card Being Low: ", round(((self.totalHigh2 / self.total2)*100), 2), "%" )
+        
+        
 class Player:
     def __init__(self):
         pass
@@ -119,5 +182,6 @@ while True:
     counter.assignment(card)
     counter.TrueCount()
     counter.RunningCOunt()
+    
     del card
 
